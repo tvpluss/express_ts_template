8686d2af888cab7d7cd048a415ae6622431e97db
tvphuocst <truongphuocst310301@gmail.com>
2023-07-13 14:33:38 +0700

    update

--- src/config/development.ts
+++ src/config/development.ts
@@ -2,17 +2,17 @@ export const development_config = {
   mode: `development`,
   db: {
     host: "localhost",
-    database: "qz_template",
+    database: "template",
     port: 3306,
     username: "root",
-    password: "Phuoc123@",
+    password: "postgres",
     seekAccount: false,
     syncForce: false,
     syncAlter: true,
     seek: {
       username: "a",
       password: "123",
-      companyName: "QZT",
+      companyName: "Tem",
     },
   },
 };
